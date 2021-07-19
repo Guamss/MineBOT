78793470b7e9dc5e8a1837066007ae9ff8e085ec
Guams <guamsmdr@gmail.com>
2021-07-19 15:50:08 +0200

    MineBOT 1.2

--- cog_basic.py
+++ cog_basic.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import youtube_dl
-from music import Video
+import random
+from opgg_scraper.monscraper import Opggtrack
 
 class CogBasic(commands.Cog):
     def __init__(self, bot):
@@ -23,12 +23,25 @@ class CogBasic(commands.Cog):
             await ctx.send("I need to be in a voice channel to do this.")
         await ctx.message.guild.voice_client.disconnect()
         await ctx.send("Poof ! Thanks for using me !")
+        
+    @commands.command()
+    async def punch(self, ctx):
+        gifs = ["gif/punch/punch0.gif", "gif/punch/punch1.gif", "gif/punch/punch2.gif"]
+        random.shuffle(gifs)
+        await ctx.send("Vous tabassez (à mort) un gros fils de pute", file=discord.File(gifs[0]))
+        
+    @commands.command()
+    async def commandlist(self, ctx):
+        await ctx.send(file = discord.File("help/help.txt"))
 
-        music= {}
+    @commands.command()
+    async def opgg(self, ctx, username, region):
+        tracker = Opggtrack(username, region)
+        await ctx.send(f"{username} en {region}")
+        await ctx.send(f"rank : {tracker.get_ranksolo()} | Classé Solo/Duo")
+        await ctx.send(f"rank : {tracker.get_rankflex()} | Flex 5:5")
+        await ctx.send(f"winrate : {tracker.get_winratio()}")
+        await ctx.send(f"kda ratio : {tracker.get_kdaratio()}")
+        
 
-        @commands.command()
-        async def play(self, ctx, url):
-            if url.starstwith("https"):
-                if url.startswith("https://www.youtube.com/"):
-                    client = ctx.guild.voice_client
-        #Commande à finir...
+        
--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions
 import cog_admin
 import cog_basic
+from opgg_scraper.monscraper import Opggtrack
 
 bot = commands.Bot(command_prefix ="__", allowed_mentions =  discord.AllowedMentions(everyone = True, users = True))
 @bot.event
 async def on_ready():
-    print("MineBOT est prêt à l'usage !")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="with a lot of users"))
+    print("MineBOT state : Ready")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="__commandlist for command list"))
     
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("Looks like you are not allowed to do this...")
+
+    
 
 bot.add_cog(cog_basic.CogBasic(bot))
 bot.add_cog(cog_admin.CogAdmin(bot))
